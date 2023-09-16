720065fc37b72652b0dbbd17d305a086d86fad2c
sanjaydell <sanjay.anuchuri@gmail.com>
2023-09-16 13:45:25 +0530

    created scaffolding and backgound

---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Login.scss";
+// import background from '/public/background.png'
+
+function Login() {
+  return (
+    <div
+      className="login"
+      style={{
+        backgroundImage: `url(/background.png)`,
+      }}
+    >
+      Login
+    </div>
+  );
+}
+
+export default Login;
