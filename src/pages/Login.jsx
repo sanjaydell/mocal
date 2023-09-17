49e7bced807e68859b264add1573d59e9fe2cdde
sanjaydell <sanjay.anuchuri@gmail.com>
2023-09-17 13:30:41 +0530

    Created login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Login.scss";
+import Card from "@mui/material/Card";
+import { Button, Link, TextField } from "@mui/material";
 // import background from '/public/background.png'
 
 function Login() {
@@ -10,7 +12,31 @@ function Login() {
         backgroundImage: `url(/background.png)`,
       }}
     >
-      Login
+      <Card className="card">
+        <img
+          src="/logo.png"
+          alt="logo"
+          style={{ width: "156px", height: "80px" }}
+        />
+        <TextField id="standard-basic" label="Enter Username/ Email Address" variant="standard" />
+        <TextField id="standard-basic" label="Enter Password" variant="standard" />
+        <Link>Forgot Password?</Link>
+        <Button variant="contained">Log in</Button>
+        <div className="or" style={{ display: "flex", flexDirection: "row" }}>
+          <hr />
+          OR
+          <hr />
+        </div>
+        <Button variant="outlined" startIcon={() => {}}>
+          Sign Up with Google
+        </Button>
+        <Button variant="outlined" startIcon={() => {}}>
+          Sign Up with Microsoft
+        </Button>
+        <span>
+          Already have an account? <Link>Log in</Link>
+        </span>
+      </Card>
     </div>
   );
 }
